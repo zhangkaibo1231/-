88665a7d4fd0c5292f08cc1d728e6c4407286104
minakami-de-chine <74372279+zhangkaibo1231@users.noreply.github.com>
2024-06-14 19:36:34 +0800

    Add files via upload

--- others/work_space.py
+++ others/work_space.py
@@ -2,37 +2,39 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-# lebai 机器人臂的数据。根据你的仿真环境修改这些常量。
-d = [0, 0.216363, 0, 0, 0.119808, 0.098406, 0.083254]
-a = [0, 0, -0.280793, -0.259692, 0, 0]
-alpha = [0, np.pi / 2, 0, 0, np.pi / 2, -np.pi / 2]  # lebai 机器人的alpha参数（以弧度为单位）
-theta = np.zeros((9, 7))  # 存储8组解决方案，每组6个角度
+def fkr(ang):
+    d = [0, 0.216363, 0, 0, 0.119808, 0.098406, 0.083254 ]
+    a = [0, 0, -0.280793, -0.259692, 0, 0, 0]
+    alpha = [0,  np.pi / 2, 0, 0, np.pi / 2, -np.pi / 2, 0]  # lebai 机器人的alpha参数（以弧度为单位）
+    #theta = np.zeros(7)  # 每个关节的角度位置
+    #theta[1], theta[2], theta[3], theta[4], theta[5], theta[6] = [float(val) for val in input("请输入各关节角度 1, 2, 3, 4, 5, 6:\n").split()]
+    ang = np.array(ang)  # 把输入参数转换为numpy数组
+    theta = np.insert(ang, 0, 0)  # 在index为0的位置插入0
 
-def kinematics(theta_input):
-    T = [np.eye(4) for _ in range(7)]  # 将变换矩阵初始化为单位矩阵
-
-    # 计算每个关节角度的单独变换矩阵
+    # 计算关节之间的变换矩阵
+    T = [np.eye(4)]
     for i in range(1, 7):
-        cos_theta = np.cos(theta_input[i])
-        sin_theta = np.sin(theta_input[i])
-        cos_alpha = np.cos(alpha[i - 1])
-        sin_alpha = np.sin(alpha[i - 1])
-        T[i] = np.array([
-            [cos_theta, -sin_theta, 0, a[i - 1]],
-            [sin_theta * cos_alpha, cos_theta * cos_alpha, -sin_alpha, -sin_alpha * d[i]],
-            [sin_theta * sin_alpha, cos_theta * sin_alpha, cos_alpha, cos_alpha * d[i]],
+        cos_theta = np.cos(theta[i])
+        sin_theta = np.sin(theta[i])
+        cos_alpha = np.cos(alpha[i])
+        sin_alpha = np.sin(alpha[i])
+        T.append(np.array([
+            [cos_theta, -sin_theta*cos_alpha, sin_theta*sin_alpha, a[i]*cos_theta],
+            [sin_theta , cos_theta * cos_alpha, -cos_theta*sin_alpha, sin_theta * a[i]],
+            [0,  sin_alpha, cos_alpha, d[i]],
             [0, 0, 0, 1]
-        ])
+        ]))
 
-    # 计算从基座到末端执行器的最终变换矩阵
-    T06 = np.linalg.multi_dot(T[1:])  # 所有T矩阵的点积
+    # 计算相对于基坐标的变换矩阵
+    trans =  np.linalg.multi_dot(T[1:])
+    #trans = np.dot(T[1], np.dot(T[2], np.dot(T[3], np.dot(T[4], np.dot(T[5], T[6])))))
 
-    # 输出末端执行器的位置，用于验证
-    X = T06[0, 3]
-    Y = T06[1, 3]
-    Z = T06[2, 3]
+    X=trans[0,3]
+    Y=trans[1,3]
+    Z=trans[2,3]
 
-    return X, Y, Z
+
+    return X,Y,Z
 
 
 xs = []
@@ -43,7 +45,7 @@ zs = []
 samples = np.random.uniform(low=-2*np.pi, high=2*np.pi, size=(100000, 7))
 
 for sample in samples:
-    x, y, z = kinematics(sample)
+    x, y, z = fkr(sample)
     xs.append(x)
     ys.append(y)
     zs.append(z)
